e8b9d9c1c5a7a119687b8d6729e17fcd2a3adabd
hadikp <hp20001025@gmail.com>
2020-11-12 13:47:16 +0100

    Switch-case

---
+++ switch.js
@@ -0,0 +1,26 @@
+'use strict';
+
+function getLetter(s) {
+    let letter;
+    
+    switch (true) {
+        case 'a, e, i, o, u'.includes(s[0]):
+            letter = 'A';
+            break;
+        case 'b, c, d, f, g'.includes(s[0]):
+            letter = 'B';
+            break;
+        case 'h, j, k, l, m'.includes(s[0]):
+            letter = 'C';
+            break;
+        case 'n, p, q, r, s, t, v, w, x, y, z'.includes(s[0]):
+            letter = 'D';
+            break;
+            
+    }
+    console.log(letter);
+    //return letter;
+}
+let s = 'hdfgt';
+getLetter(s);
+console.log('--------------------');
